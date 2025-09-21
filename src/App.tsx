3892ba95dc1f73254baab530f72f7000f891d13e
Govind Nair <govindbinoj@gmail.com>
2025-09-22 03:59:40 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
+// src/App.tsx
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import PrivateRoute from "./components/PrivateRoute";
-import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
+        <Route path="/dashboard" element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        } />
+        {/* Add other routes */}
       </Routes>
     </BrowserRouter>
   );
