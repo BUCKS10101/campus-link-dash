c92ca211ab858d23f5c90bcc0f84e88fad4928f1
Govind <govindbinoj@gmail.com>
2025-09-22 05:16:04 +0530

    prev

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 import type { Database } from '@/types/database'
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.SUPABASE_URL
+const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
 
