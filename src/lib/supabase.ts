b80c433494476d7ed3056339a022eca15bce8221
Govind Nair <govindbinoj@gmail.com>
2025-09-22 00:25:02 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 import type { Database } from '@/types/database'
 
-const supabaseUrl = 'https://kjsseqlmnmjuqepfmldh.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtqc3NlcWxtbm1pdXFlcGZtbGRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0NTYyMDEsImV4cCI6MjA3NDAzMjIwMX0.uZbc-o__zev3uQOVrXESfArDSbow1XfC9vdXWmO_e0Q'
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
